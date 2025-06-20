bf9ad4e165ca3ff382e4100cca5d6e74e3ea5f7d
agopdev <alonso@agop.dev>
2025-06-19 18:41:16 -0600

    Rename db name

--- notif-service/src/db.rs
+++ notif-service/src/db.rs
@@ -3,6 +3,6 @@ use crate::models::Notification;
 
 pub async fn init() -> Result<Collection<Notification>> {
     let client = Client::with_uri_str("mongodb://mongo:27017").await?;
-    let db = client.database("notificaciones");
+    let db = client.database("microservices");
     Ok(db.collection::<Notification>("notifs"))
 }
